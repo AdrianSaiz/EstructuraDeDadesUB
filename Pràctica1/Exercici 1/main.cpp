48eaf51bb0d32def173527160b3442009eeb3522
Rubén Ballester Bautista <you@example.com>
2016-03-12 21:08:04 +0100

    Pràctica 1 Exercici 0 totalment funcional

--- Pràctica1/Exercici 1/main.cpp
+++ Pràctica1/Exercici 1/main.cpp
@@ -10,50 +10,75 @@
 #include "ArrayStack.h"
 
 using namespace std;
-
+/**
+ * It pushes to an ArrayStack object the opposite element of the char which has been given by parameter.
+ * @param pilaAux2
+ * @param top
+ */
+void asignaValor(ArrayStack &pilaAux2, char top) {
+    switch (top) {
+        case '[' :
+            pilaAux2.push(']');
+            break;
+        case ']' :
+            pilaAux2.push('[');
+            break;
+        case '(' :
+            pilaAux2.push(')');
+            break;
+        case ')' :
+            pilaAux2.push('(');
+            break;
+        case '{' :
+            pilaAux2.push('}');
+            break;
+        case '}' :
+            pilaAux2.push('{');
+            break;
+    }
+}
+/**
+ * It checks if a sequence of characters have the correct order to do a arithmetic operation.
+ * @param pila The stack that is going to be analyzed
+ * @return true If the expression have the correct order | false If the expression doesn't have the correct order.
+ */
 bool comprovaExpresio (ArrayStack &pila) {
+    //If the stack is empty, it returns true because there isn't elements to analyze.
     if(!pila.empty()) {
-        //If the stack is not empty
+        /**
+         * If the stack is not empty, we proceed to do the next algorithm:
+         * Two elements: pila (the original stack) pilaAux2 (a auxiliary stack to do the proper operations)
+         * 1) If "pilaAux2" is empty, we transfer the last char of "pila" to "pilaAux2" and we delete this char from "pila". We go directly to step 4.
+         *    If "pilaAux2" is not empty, we go to the step 2:
+         * 2) We check if the top of pilaAux2 is equal to the top of "pila". If it is true, we delete the char from "pilaAux2" and "pila" and we go directly to step 4. If it is not true, we go to the step 3,:
+         * 3) We transfer the opposite element to pilaAux2 from pila. We go to the step 4.
+         * 4)If pila is not empty, we do again the process.
+         * 
+         * We can prove that the loop finishes. If the size of the stack is n and also the size of the stack is a positive integer 
+         * this implies that we have a size n that follow the next rule, 0<=n. If n = 0 we stop. If not, we have that in each loop we decrement 
+         * the size of stack in 1 so we have a size of n-1. If n-1 = 0 pila is empty, and the algorithm stops, 
+         * if not, we do again the same process until we have n=0. QED
+         * 
+         * If pilaAux2 is empty, that implies the expression is correct.
+         */
         ArrayStack pilaAux2;
-        ArrayStack pilaAux1;
-        pilaAux1 = pila;
-        while(!pilaAux1.empty()) {
-            switch (pilaAux1.top()) {
-                case '[' :
-                    pilaAux2.push(']');
-                    break;
-                case ']' :
-                    pilaAux2.push('[');
-                    break;
-                case '(' :
-                    pilaAux2.push(')');
-                    break;
-                case ')' :
-                    pilaAux2.push('(');
-                    break;
-                case '{' :
-                    pilaAux2.push('}');
-                    break;
-                case '}' :
-                    pilaAux2.push('{');
-                    break;
-            }
-            pilaAux1.pop();
-        }
-        bool trobat = true;
-        pilaAux2.print();
-        while (!pila.empty() && !pilaAux2.empty() && trobat) {
-            if(pila.top() == pilaAux2.top()) {
-                pila.pop();
-                pilaAux2.pop();
+        while(!pila.empty()) {
+            if(pilaAux2.empty()) {
+                asignaValor(pilaAux2, pila.top());
             }
             else {
-                trobat = false;
+                if(pilaAux2.top() == pila.top()) {
+                    pilaAux2.pop();
+                }
+                else {
+                    asignaValor(pilaAux2, pila.top());
+                }
             }
+            pila.pop();
         }
-        if (trobat) {
+        if (pilaAux2.empty()) { 
             return true;
-        }
+        } 
         else {
             return false;
         }
@@ -63,9 +88,11 @@ bool comprovaExpresio (ArrayStack &pila) {
     }
 }
 void getStackFromInput(ArrayStack &pila) {
+    string stringAux;
     char aux;
     cout << "Introduce a numeric expresion" << endl;
-    while(cin.get(aux)) {
+    std::getline (std::cin,stringAux);
+    for(char aux: stringAux) {
         //Control d para parar, por ahora
         if (aux == '{' || aux == '}' || aux == '[' || aux == ']' || aux == '(' ||aux == ')') {
             pila.push(aux);
@@ -83,4 +110,3 @@ int main(int argc, char** argv) {
     }
     return 0;
 }
-
