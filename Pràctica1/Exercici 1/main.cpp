cc1441ce014719845db9d6619b52338721da1015
Rubén Ballester Bautista <you@example.com>
2016-03-14 18:25:36 +0100

    Bugs fixed

--- Pràctica1/Exercici 1/main.cpp
+++ Pràctica1/Exercici 1/main.cpp
@@ -11,31 +11,24 @@
 
 using namespace std;
 /**
- * It pushes to an ArrayStack object the opposite element of the char which has been given by parameter.
- * @param pilaAux2
- * @param top
+ * It returns if top is the opposite of top2. top2 represents the elements of stack.
+ * @param top1, top of expression
+ * @param top2, top of stack
  */
-void asignaValor(ArrayStack &pilaAux2, char top) {
-    switch (top) {
-        case '[' :
-            pilaAux2.push(']');
-            break;
+bool topAparellat(char top1, char top2) {
+    bool isClosed = false;
+    switch (top2) {
         case ']' :
-            pilaAux2.push('[');
-            break;
-        case '(' :
-            pilaAux2.push(')');
+            isClosed = (top1 == '[') ? true : false;
             break;
         case ')' :
-            pilaAux2.push('(');
-            break;
-        case '{' :
-            pilaAux2.push('}');
+            isClosed = (top1 == '(') ? true : false;
             break;
         case '}' :
-            pilaAux2.push('{');
+            isClosed = (top1 == '{') ? true : false;
             break;
     }
+    return isClosed;
 }
 /**
  * It checks if a sequence of characters have the correct order to do a arithmetic operation.
@@ -50,7 +43,7 @@ bool comprovaExpresio (ArrayStack &pila) {
          * Two elements: pila (the original stack) pilaAux2 (a auxiliary stack to do the proper operations)
          * 1) If "pilaAux2" is empty, we transfer the last char of "pila" to "pilaAux2" and we delete this char from "pila". We go directly to step 4.
          *    If "pilaAux2" is not empty, we go to the step 2:
-         * 2) We check if the top of pilaAux2 is equal to the top of "pila". If it is true, we delete the char from "pilaAux2" and "pila" and we go directly to step 4. If it is not true, we go to the step 3,:
+         * 2) We check if the top of pilaAux2 is equal to the opposite of top of "pila" (Note that we only check the closing expression). If it is true, we delete the char from "pilaAux2" and "pila" and we go directly to step 4. If it is not true, we go to the step 3,:
          * 3) We transfer the opposite element to pilaAux2 from pila. We go to the step 4.
          * 4)If pila is not empty, we do again the process.
          * 
@@ -60,18 +53,20 @@ bool comprovaExpresio (ArrayStack &pila) {
          * if not, we do again the same process until we have n=0. QED
          * 
          * If pilaAux2 is empty, that implies the expression is correct.
+         * The complexity function of this algorithm is of O(n). This is because we have O(n) (we create the first stack) + O(n) (we check all elements of first stack) + O(1) (cte senteces) = 3O(n) = O(n)
          */
         ArrayStack pilaAux2;
         while(!pila.empty()) {
+            
             if(pilaAux2.empty()) {
-                asignaValor(pilaAux2, pila.top());
+                pilaAux2.push(pila.top());
             }
             else {
-                if(pilaAux2.top() == pila.top()) {
+                if(topAparellat(pila.top() , pilaAux2.top())) {
                     pilaAux2.pop();
                 }
                 else {
-                    asignaValor(pilaAux2, pila.top());
+                    pilaAux2.push(pila.top());
                 }
             }
             pila.pop();
@@ -93,7 +88,6 @@ void getStackFromInput(ArrayStack &pila) {
     cout << "Introduce a numeric expresion" << endl;
     std::getline (std::cin,stringAux);
     for(char aux: stringAux) {
-        //Control d para parar, por ahora
         if (aux == '{' || aux == '}' || aux == '[' || aux == ']' || aux == '(' ||aux == ')') {
             pila.push(aux);
         }
